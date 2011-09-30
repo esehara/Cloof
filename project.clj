c7b2b75c7bb7666f2d92aa2397ce4ac0b978af36
esehara <esehara@gmail.com>
2011-09-30 18:56:29 +0900

    Fix Readme

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cloof "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Profile Site with Clojure for Heroku."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.6.4"]
