7023aff70f1b5224b0caea5acff2c6c86e878144
esehara <esehara@gmail.com>
2011-09-30 18:49:50 +0900

    Test Commit

---
+++ src/cloof/prof.clj
@@ -0,0 +1,62 @@
+(ns cloof.prof)
+
+(def whoisyou
+  {:name "似非原重雄"
+   :description "貴方の人生のお荷物です。"
+   :twitter "esehara"
+   :blog "http://bugrammer.g.hatena.ne.jp/nisemono_san/" 
+   :blog-name "蟲！虫！蟲！" 
+   :e-mail "esehara at gmail dot com"})
+
+(def worklist
+  '(
+    ["R.I.P esehara" "http://r-i-p-e-s-e-h-a-r-a.appspot.com/"
+     "Google App Engine for Javaで作成。基本的にJavaの勉強の代わりに作られたもの。Twitterにおいて、任意に選ばれた発言を、他人のアカウントでつぶやき続けるというもの。"]
+    ["悲しみよ、こんにちは" "http://esehara.dotera.net/Study/html5/canvas.html"
+     "Twitterの発言から「死にたい」を検索し、表示する。HTML5(canvas) + Javascript(jQuery)の勉強のために作ったサイト。"]
+    ["ROBOTS" "http://www.pyweek.org/e/esehara/" "Pyweekと呼ばれるPythonのゲームプログラミングに参加したときに作ったもの。とはいえ、殆どの挙動は知人のプログラムが作ったものであり、ゲーム的に面白いか？といえば……。Python製。"]
+    ["aNegi" "https://github.com/esehara/aNegi" "『ねぎ姉さん』のサイトにアップされている四コマを見るためのアンドロイドアプリ。サンプルを製作する程度に作ったもの。Java製。"]
+    )
+)
+
+(def bloglist
+  '(
+    ["微力" "http://b.hatena.ne.jp/nisemono_san/" "はてなブックマークです。自分が興味を持った話題は、できるだけこちらに集約させるようにしています。"]
+    ["蟲！虫！蟲！" "http://bugrammer.g.hatena.ne.jp/nisemono_san/" "プログラム学習サイト。自分の学習目的のため、間違いも色々とあるようす。"]
+    ["行乞記" "http://fragments.g.hatena.ne.jp/nisemono_san/" "自分が日々思っていたことをメモするためのサイト。"]
+    ["SoundCloud - esehara" "http://soundcloud.com/esehara" "過去に自分が作った音楽みたいなもののリストです。"]
+    ["Sayonara Records" "http://sayonararecords.tumblr.com/" "無料で音楽配信を行うネットレーベル。Tumblr上に存在。"]
+    ["似非原路上日記。" "http://togetter.com/li/60272" "ホームレス時代の記憶。"]
+    ["Favstar.fm - esehara" "http://favstar.fm/users/esheara" "過去に自分がつぶやいたもの。"]
+    )
+)
+
+(def gistlist
+  '(
+    ["Nise Hack!!" "http://bugrammer.g.hatena.ne.jp/nisemono_san/20110919/1316400120" "Javascript、特にブックマークレットを使用した場合における。あたかもサイトが映画に出てくるようなハッカーのサイトのように表示される。"]
+    ["社会学評論Download" "https://gist.github.com/1088146" "社会学評論で公開されている過去の記事を全てダウンロードするスクリプト。Python製。"]
+    ["Aozora Author 2 epub" "https://gist.github.com/1101065" "そういえば、青空文庫って作品別のEpubはあるけれど、作者の著作全てをまとめてEpubにしたものはなかったな、ということで作成。実際の挙動はXHTMLで公開されているものだけを集めてくるという、至極手抜きのスクリプト。Python製"]
+    )
+)
+
+(def nikolist
+  '(
+    ["眠れないので作っていたら「作者は病気」っぽくなったゲーム" "http://www.nicovideo.jp/watch/sm6416309" "自分が実家で引きこもっていたときに作られたゲーム。グロテスクで怖い雰囲気なので、苦手な人は苦手かもしれません。このゲームは『AzDesignADV』というソフトで作られています。"]
+    ["途中まで作った「作者は病気」っぽいゲームを自分でプレイ" "http://www.nicovideo.jp/watch/sm6300734" "これも上と同じようなゲームです。これも、グロテスクで怖い感じです。製作ツールは、「Game Maker 7.0」です。海外では結構有名なツールのようです。"]
+    )
+)
+
+(def iroiro
+  '(["好きな言語はなんですか？" "Python、Javascript、Clojure辺りですね。Pythonは書いていてスッキリしますね。JavascriptはPrototype型のオブジェクト指向で、他のオブジェクト指向言語とはまた違った発想をしなきゃいけないことが面白いですね。Clojureは最近始めたばっかりで、Lispを勉強するがてら始めたのですが、Lispの表現がこれほどまでに強力であるとは知りませんでした。触っていて気もちいい言語の一つです。"]
+    ["何に興味がありますか？" "元々は社会学を勉強していました。その一方で、音楽を作ったり、絵を描いたりすることにも興味があったりします。個人的には、ドット絵が好みです。最近では「技術と人間は如何に関わるのか」「人間は如何にして情報を得られるのか」の観点から、プログラミング等の勉強を行っています。"])
+  )
+
+(def thankslist
+  '(["Free Material Makochin" "http://members3.jcom.home.ne.jp/i.makochin/" "素材サイト。切り取り線はここから頂きました。ありがとうございます。"])
+  )
+
+(def site-configure 
+  {:title "似非原Profile"
+   :person whoisyou})
+
+
